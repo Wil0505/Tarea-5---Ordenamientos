0d0c06eb26a24462001f137ce0f259804f47885c
Wilmer Contreras <winteriano17@gmail.com>
2022-04-03 18:58:21 -0600

    Se agregar los metodos HeapSort, MergeSort y ShellSort

---
+++ HeapSort.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+class MinHeap
+{
+		public:
+			int *harr;
+			int capacity;
+			int heap_size;
+			
+			MinHeap(int cap)
+			{
+				heap_size = cap;
+				capacity = cap;
+				harr = new int[cap];
+			}
+			
+	void imparray()
+	{
+		for(int i=0; i<heap_size; i++)
+			cout<<harr[i]<<" ";
+			cout<<endl;
+	}	
+		int parent(int i)
+		{
+			return (i-1)/2;
+		}
+		
+		int left(int i)
+		{
+			return (2*i + 1);
+		}
+		
+		int right(int i)
+		{
+			return (2*i + 2);
+		}
+		
+		int getMain()
+		{
+			return harr[0];
+		}
+		
+	void MinHeapify(int i)
+	{
+		int l = left(i);
+		int r = right(i);
+		int smallest = i;
+		if(l < heap_size && harr[l] < harr[i])
+			smallest = l;
+		if(r < heap_size && harr[l] < harr[smallest])
+			smallest = r;
+		if(smallest != i)
+		{
+			swap(harr[i], harr[smallest]);
+			MinHeapify(smallest);
+		}
+	}
+	
+	int extractMin()
+	{
+		if(heap_size <= 0)
+			return INT_MAX;
+		if(heap_size == 1)
+		{
+			heap_size--;
+			return harr[0];
+		}
+		
+		int root = harr[0];
+		harr[0] = harr[heap_size-1];
+		heap_size--;
+		MinHeapify(0);
+		
+		return root;
+	}
+	
+	void getUnsortedArray()
+	{
+		cout << "Ingrese los "<<capacity<<" que desea ingresar: "<<endl;
+		for(int i=0; i<capacity; i++)
+		cin>>harr[i];
+	}
+	
+	void heap()
+	{
+		int temp[capacity];
+		for(int j=0; j<capacity; j++)
+		{
+			temp[j]=extractMin();
+			cout<<temp[j]<<" ";
+		}
+	}
+};
+
+void HeapSort()
+{
+	int s;
+	cout<<"Ingrese el tamanio"<<endl;
+	cin>>s;
+	MinHeap obj(s);
+	obj.getUnsortedArray();
+	
+	cout<<"Datos desordenados: "<<endl;
+	obj.imparray();
+	
+	for(int i=s/2-1; i>=0; i--)
+	{
+		obj.MinHeapify(i);
+	}
+	
+	cout<<"Datos ordenados: "<<endl;
+	obj.heap();
+}
